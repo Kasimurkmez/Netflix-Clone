8e1512dfa816edd9aaa52ddd3631fd2910df935a
kasimurkmez <kasimurkmez47@gmail.com>
2023-06-28 20:42:37 +0300

    updated

--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -4,7 +4,7 @@ import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { useEffect, useState } from "react";
 import Movie from "./Movie";
 
-const Row = ({ title, fetchURL }) => {
+const Row = ({ title, fetchURL, rowID }) => {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
@@ -13,16 +13,26 @@ const Row = ({ title, fetchURL }) => {
     });
   }, [fetchURL]);
 
+  const slideLeft = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft - 500;
+  };
+  const slideRight = () => {
+    var slider = document.getElementById("slider" + rowID);
+    slider.scrollLeft = slider.scrollLeft + 500;
+  };
+
   return (
     <>
       <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
       <div className="relative flex items-center group">
         <MdChevronLeft
+          onClick={slideLeft}
           size={40}
           className="bg-white left-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
         />
         <div
-          id={"slider"}
+          id={"slider" + rowID}
           className="w-full h-full overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
         >
           {movies.map((item, id) => (
@@ -30,6 +40,7 @@ const Row = ({ title, fetchURL }) => {
           ))}
         </div>
         <MdChevronRight
+          onClick={slideRight}
           size={40}
           className="bg-white right-0 rounded-full absolute opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
         />
