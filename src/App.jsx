dc70391e58d873423c50a9b440193cc3175fa614
kasimurkmez <kasimurkmez47@gmail.com>
2023-07-02 23:35:54 +0300

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,9 @@ import Navbar from "./components/Navbar";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import { AuthContextProvider } from "./context/Authcontext";
+import Account from "./pages/Account";
+import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -10,6 +13,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signup" element={<Account />} />
         </Routes>
       </AuthContextProvider>
     </>
--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { auth } from "../pages/Firebase";
+import { auth } from "../firebas";
 import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
